4b2a6ae51c2b2e0f55edbd0b7d485ae0bfeb9115
Arun_prasanth <yabadabadiiduu@gmail.com>
2025-10-24 12:47:51 +0400

    py learning 1

---
+++ data types and variables/Data_types.py
@@ -0,0 +1,45 @@
+a = 7   # Integer data_type
+
+d  = 3.14   # Float data_type
+
+n = "sherpa"    # String data_type
+
+b = False   #Boolean data_type
+
+#Python type function
+
+print(type(a)) # Type() function is used to find the data type of the value stored in the variable
+
+print(type(d))
+
+print(type(n))
+
+print(type(b))
+
+print(a)
+
+#python concatenation
+
+print(a+d)  #concatenating 2 variable
+
+print(10+190)   #concatenating 2 integer
+
+print(10.9961+2.855) #concatenating 2 float
+
+print("Hello"+"world")    #concatenating 2 string - Classic print statement
+
+'''print("Hello world")'''  # triple quote is another way of commenting the code
+
+print(True+a)   #we can concatenate the boolean values also because the value of true = 1 and false = 0
+
+print(True+10)  #concatenating integer and boolean
+
+# print(True+"JOJO") #concatenating string and boolean (Not possible concatenation between string and boolean)
+#
+# print(25+"JOJO") #concatenating int and boolean (Not possible concatenation between int and boolean)
+
+print(n)
+
+del n   # del function is used for deleting the variable from the script
+
+"""print(n)"""
