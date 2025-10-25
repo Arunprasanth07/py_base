3de1d9d73e627f5595cc9395054c3872ba9021d9
Arun_prasanth <yabadabadiiduu@gmail.com>
2025-10-25 18:29:28 +0400

    Conditional statement

---
+++ conditional_statements_and_operators/conditional_statements.py
@@ -0,0 +1,42 @@
+#Conditional statements
+
+#If condition
+
+mark  = 45
+
+if mark > 35:
+        print("You are pass")
+
+#if AND Else condition
+
+mark2 = 80
+
+if mark2 < 35:
+    print("You failed")
+else:
+    print("You are passed, I think")
+
+# if False:
+#     print("Yes, it's True")
+# else:
+#     print("No, it's False")
+
+#Elif Condition
+
+mark5 =68
+
+if mark5 < 35:
+    print("You failed")
+
+elif mark5 > 35:
+    print("You are B, Bruh")
+
+elif mark5 > 50:
+    print("You are B+, Bro")
+
+elif mark5 > 70:
+    print("You are A, Not bad")
+elif mark5 > 80:
+    print("You are A, I mean It's Ok")
+elif mark5 > 90:
+    print("You are O, Your are a god i say")
