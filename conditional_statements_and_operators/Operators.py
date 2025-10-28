51e3a21314ce94588058dfb1ceb41a0a9a349bf0
Arun_prasanth <yabadabadiiduu@gmail.com>
2025-10-28 13:30:03 +0400

    Operators

---
+++ conditional_statements_and_operators/Operators.py
@@ -0,0 +1,55 @@
+#Arithmentic Operators
+
+a = 5
+z = 15
+
+add = a + z # "+" Addition Operator
+sub = a - z # "-" subtract Operator
+multi = a * z # "*" Multiplication Operator
+div = a / z # "/" Division Operator
+div2 = a // z # "/" Division Operator only return the Quotient
+mod = a % z # "%" Division Operator only return the Remainder
+root = a**z # Square root value
+
+print(add)
+print(sub)
+print(multi)
+print(div)
+print(div2)
+print(mod)
+print(root)
+#Relational Operators
+
+m = 0
+
+n = 1
+
+greater = m > n
+lesser = m < n
+equals = m == n
+notequ = m != n
+graequals = m >= n
+lessequals = m <= n
+
+print(greater, lesser, equals)
+print(notequ)
+print(graequals)
+print(lessequals)
+
+#Logical operators
+
+b = True
+
+c = False
+
+And = b and c
+Or = b or c
+Notb = not b
+Notc = not c
+
+print("---------------------------------------------------")
+
+print(And)
+print(Or)
+print(Notb)
+print(Notc)
