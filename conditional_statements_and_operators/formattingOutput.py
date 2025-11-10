90830879d45cd57f7aea04cb29d878c140f3db7b
Arun_prasanth <yabadabadiiduu@gmail.com>
2025-11-10 19:12:53 +0400

    Formatting op

---
+++ conditional_statements_and_operators/formattingOutput.py
@@ -0,0 +1,15 @@
+Name= 'Arun'
+
+Age = 28
+
+role = 'Gypsy'
+
+Name, Age, role = 'Arun',999,"error"
+
+print(Name)
+print(Age)
+print(role)
+
+print(Name, Age, role)
+
+print( "Name is %s, Age is %d, role is %s" %(Name,Age,role))
